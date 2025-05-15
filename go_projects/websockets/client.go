9c5702f5f5dfd930900a24b922a40246ecbb1387
Chris-Mwiti <chrismwiti21@gmail.com>
2025-05-15 17:56:37 +0300

    modifications: writing the client write socket method

--- go_projects/websockets/client.go
+++ go_projects/websockets/client.go
@@ -1,7 +1,6 @@
 package websockets
 
 import (
-	"bytes"
 	"log"
 	"net/http"
 	"time"
--- go_projects/websockets/ui.go
+++ go_projects/websockets/ui.go
@@ -1,17 +1,28 @@
 package websockets
 
 import (
+	"errors"
 	"log"
+	"net/http"
 	"time"
-
-	"github.com/google/uuid"
+	uuid "github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
 
 type status string
 
 const (
 	Offline status = "Offline"
-	Online status = "online"
+	Online status = "Online"
+	Typing status = "Typing"
+)
+
+const (
+	readDeadline = 10 * time.Second 
+	writeDeadline = 5 * time.Second 	
+	setPongWait =  10 * time.Second 
+	setPingWait = setPongWait / 4
+	readLimit = 256
 )
 
 //@todo: implement the following methods
@@ -24,7 +35,9 @@ type Room struct {
 	Name string
 	conn map[*ClientConn]status
 	messages *MessageHub
-	receive chan *messageChannel
+	broadcast chan *messageChannel
+	register chan*ClientConn
+	unregister chan*ClientConn
 }
 
 //@todo: implement the following methods
@@ -51,10 +64,13 @@ type MessageHub struct {
 
 //@todo: implement the following methods
 //1. Create a new conn
+//2. Attach a conn to a room
 //2. Send data over a connection stream
 type ClientConn struct {
 	Id string
 	Rooms []*Room
+	activeRoom *Room
+	Conn *websocket.Conn
 	send chan *Message
 }
 
@@ -67,36 +83,136 @@ func NewRoom(rn string) *Room{
 		Name: rn,
 		conn: make(map[*ClientConn]status),	
 		messages: new(MessageHub),
-		receive: make(chan *messageChannel),
+		broadcast: make(chan *messageChannel),
 	} 
 	return room
 }
 
+func (room *Room)SetConnStatus(client *ClientConn,conn status) {
+	room.conn[client] = conn
+}
 
-
-func NewConn(room string) *ClientConn{
+func NewConn(room string, w http.ResponseWriter, r *http.Request) (*ClientConn, error){
 	id := uuid.NewString()
 
-	conn := &ClientConn{
+
+	//@todo: Implent a cli gui form and option selector available rooms
+
+	//new room declaration
+	nr := NewRoom(room)
+	//update the status of the client in the room
+
+	//upgrade the current http conn to a websocket conn
+	upgrader := websocket.Upgrader{
+		ReadBufferSize: 1024,
+		WriteBufferSize: 1024,
+	}
+
+	conn, err := upgrader.Upgrade(w, r, r.Header)
+
+	if err != nil {
+		return nil, errors.Join(errors.New("error while establishing connection"), err)
+	}
+	
+
+	roomConn := &ClientConn{
 		Id: id,
-		Rooms: make([]*Room, 0),
+		Conn: conn,
 		send: make(chan *Message),
 	}
 
-	return conn
+	//append the newly created room
+	roomConn.appendRoom(nr)
+	//set the createdRoom to be the active one
+	roomConn.setActiveRoom(nr)
+
+	//adjust the connection of the connection 
+	nr.SetConnStatus(roomConn, Online)
+
+	return roomConn, nil
 }
+
+func (client *ClientConn) appendRoom(room *Room){
+	client.Rooms = append(client.Rooms, room)
+}
+
+func (client *ClientConn) setActiveRoom(room *Room){
+	client.activeRoom = room
+}
+
+func(client *ClientConn) ReadMessage(){
+	//set the defaults such as pingtimeouts, ponttimeouts, and close methods
+	defer func(){
+		err := client.Conn.Close()
+		if err != nil {
+			log.Printf("error while closing the client connection")
+			log.Panic(err)
+		}
+	}()
+	client.Conn.SetReadLimit(readLimit)
+	client.Conn.SetPongHandler(func(appData string) error {client.Conn.SetReadDeadline(time.Now().Add(pongWait)); return nil})
+
+	//always read for a message
+	for{
+		_,message,err := client.Conn.ReadMessage()
+
+		if err != nil{
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseAbnormalClosure, websocket.CloseGoingAway) {
+				log.Printf("error while read websocket: %v", err)
+			}
+			break
+		}
+
+		//construct the messageSendOb
+		newMsg := newChanMessage(client,message)
+		client.activeRoom.broadcast <- newMsg 
+	}
+}
+
+func (client *ClientConn) WriteMessage(){
+	if _,ok :=<-client.send; ok{
+	  log.Panicf("client send channel has been closed")
+	}
+
+	for message := range client.send {
+			
+	}
+}
+
+func newChanMessage(client *ClientConn, message []byte) *messageChannel{
+	id := uuid.NewString() 
+	return &messageChannel{
+		sender: client,
+		message: &Message{
+			Id: id,
+			timestamp: time.Now(),
+			data: message,	
+		},
+	}
+}
+
 
 //always listens for incoming messages
 func (room *Room) Listen(){
 	for {
 		select{
-		case message, ok:= <-room.receive:
+		case structMess, ok:= <-room.broadcast:
 		if !ok {
 				log.Println("error while receiving message")
 			}
-		//store the message for the client conn in the room
-		room.messages = &MessageHub{}		
+		//broadcast to the room users someone is typing
+		for client, _ := range room.conn{
+				err := client.Conn.SetWriteDeadline(time.Now().Add(writeDeadline))
+				if err != nil {
+					log.Panicf("error while setting write deadline: %v", err)
+				}
+				//send the message to each of the clients
+				client.send <- structMess.message
+			}
 		}
 
 	}
 }
+
+
+
