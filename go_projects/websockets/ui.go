5574a5bd4034f23c2ac33299959cb4008ee2f570
Chris-Mwiti <chrismwiti21@gmail.com>
2025-05-16 11:48:03 +0300

    changes: modifications to the room methods. attaching to a room and detaching to a room

--- go_projects/websockets/ui.go
+++ go_projects/websockets/ui.go
@@ -68,8 +68,9 @@ type MessageHub struct {
 //2. Send data over a connection stream
 type ClientConn struct {
 	Id string
-	Rooms []*Room
-	activeRoom *Room
+	Rooms map[string]*Room //will keep track of the engage rooms by the connection 
+	activeRoom *Room //will keep track of which room the conn is currently active
+	status status
 	Conn *websocket.Conn
 	send chan *Message
 }
@@ -88,20 +89,11 @@ func NewRoom(rn string) *Room{
 	return room
 }
 
-func (room *Room)SetConnStatus(client *ClientConn,conn status) {
-	room.conn[client] = conn
-}
+
 
 func NewConn(room string, w http.ResponseWriter, r *http.Request) (*ClientConn, error){
 	id := uuid.NewString()
-
-
 	//@todo: Implent a cli gui form and option selector available rooms
-
-	//new room declaration
-	nr := NewRoom(room)
-	//update the status of the client in the room
-
 	//upgrade the current http conn to a websocket conn
 	upgrader := websocket.Upgrader{
 		ReadBufferSize: 1024,
@@ -118,22 +110,30 @@ func NewConn(room string, w http.ResponseWriter, r *http.Request) (*ClientConn, 
 	roomConn := &ClientConn{
 		Id: id,
 		Conn: conn,
+		status: Online,
 		send: make(chan *Message),
 	}
-
-	//append the newly created room
-	roomConn.appendRoom(nr)
-	//set the createdRoom to be the active one
-	roomConn.setActiveRoom(nr)
-
-	//adjust the connection of the connection 
-	nr.SetConnStatus(roomConn, Online)
-
 	return roomConn, nil
 }
 
+func (client *ClientConn) AttachToRoom(rn string){
+	if _,ok := client.Rooms[rn]; !ok{
+		//now one can create the room 
+		nr := NewRoom(rn)
+		nr.conn[client] = client.status
+
+		client.appendRoom(nr)
+		client.setActiveRoom(nr)
+
+		return
+	}
+	
+	room := client.Rooms[rn]
+	client.setActiveRoom(room)
+}
+
 func (client *ClientConn) appendRoom(room *Room){
-	client.Rooms = append(client.Rooms, room)
+	client.Rooms[room.Id] = room
 }
 
 func (client *ClientConn) setActiveRoom(room *Room){
@@ -146,7 +146,6 @@ func(client *ClientConn) ReadMessage(){
 		err := client.Conn.Close()
 		if err != nil {
 			log.Printf("error while closing the client connection")
-			log.Panic(err)
 		}
 	}()
 	client.Conn.SetReadLimit(readLimit)
@@ -173,9 +172,32 @@ func (client *ClientConn) WriteMessage(){
 	if _,ok :=<-client.send; ok{
 	  log.Panicf("client send channel has been closed")
 	}
+	client.Conn.SetWriteDeadline(time.Now().Add(writeDeadline))
 
 	for message := range client.send {
-			
+		writer, err := client.Conn.NextWriter(websocket.TextMessage)			
+
+		if err != nil{
+			//check if the error is an unexpected error
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseAbnormalClosure, websocket.CloseGoingAway) {
+				log.Panicf("error while writing message: %v", err)
+			}
+			break
+		}
+
+		_,err = writer.Write(message.data)
+		if err != nil {
+			log.Panicf("error while writing to the connection: %v", err)
+		}
+
+		//send the queued data
+		for i := len(client.send); i <= 0; i--{
+			message := <-client.send
+			_,err = writer.Write(message.data)
+			if err != nil {
+				log.Panicf("error while writing to the connections: %v",err)
+			}
+		}
 	}
 }
 
@@ -196,20 +218,38 @@ func newChanMessage(client *ClientConn, message []byte) *messageChannel{
 func (room *Room) Listen(){
 	for {
 		select{
-		case structMess, ok:= <-room.broadcast:
-		if !ok {
+			case rcvMessage, ok:= <-room.broadcast:
+			if !ok {
 				log.Println("error while receiving message")
 			}
-		//broadcast to the room users someone is typing
-		for client, _ := range room.conn{
+			//update the status of the broadcaster
+			room.conn[rcvMessage.sender] = Typing
+
+			//broadcast to the room users someone is typing
+			for client,_:= range room.conn{
 				err := client.Conn.SetWriteDeadline(time.Now().Add(writeDeadline))
 				if err != nil {
 					log.Panicf("error while setting write deadline: %v", err)
 				}
 				//send the message to each of the clients
-				client.send <- structMess.message
+				client.send <- rcvMessage.message
 			}
+		case client,ok := <-room.register:
+		if !ok {
+		 log.Println("error while registering new client")	
 		}
+		//store the newly created user and update the status
+		room.conn[client] = Online
+
+	  //unregister event listener
+		case client, ok := <-room.unregister:
+		if !ok {
+				log.Println("error while unregistering client")
+		}
+		close(client.send)
+		delete(room.conn, client)
+
+	}
 
 	}
 }
