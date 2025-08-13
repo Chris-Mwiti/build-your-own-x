275d84a475a5130e8de00b487cc5d6c900004630
Chris-Mwiti <chrismwiti21@gmail.com>
2025-08-13 14:41:54 +0300

    changes: Added close channel signal shutdown

--- go_projects/Decentralized-Terminal-Social-App/main.go
+++ go_projects/Decentralized-Terminal-Social-App/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Chris-Mwiti/ChatX/logger"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/Chris-Mwiti/ChatX/logger"
 )
 
 func main (){
 	//logger initialization
 	_, sugarLogger := logger.Init()
+
+	//now lets try to reconfigure the node ip address
+	//@todo: Research more on how to customize a node settings
 	node, err := libp2p.New()
 	if err != nil {
 		panic(err)
@@ -19,6 +26,13 @@ func main (){
 		"Host Id", hostInfo.ID,
 		"Host Addr", hostInfo.Addrs,
 	)
+
+	//lets wait for a shutdown signal to close the connection
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+
+	sugarLogger.Infoln("Received signal, shutting down...")
 
 	//shut down the node when an error has occured
 	if err := node.Close(); err != nil {
