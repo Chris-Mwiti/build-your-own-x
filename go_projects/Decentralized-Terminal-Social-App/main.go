89ef97332fa5a2263f27ebb27e331e15523082bb
Chris-Mwiti <chrismwiti21@gmail.com>
2025-08-13 16:22:07 +0300

    Stage1: Accomplished to setup node communications(ping,pong)

--- go_projects/Decentralized-Terminal-Social-App/main.go
+++ go_projects/Decentralized-Terminal-Social-App/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"syscall"
@@ -8,6 +9,9 @@ import (
 	"github.com/Chris-Mwiti/ChatX/logger"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+	"github.com/multiformats/go-multiaddr"
 )
 
 func main (){
@@ -16,7 +20,11 @@ func main (){
 
 	//now lets try to reconfigure the node ip address
 	//@todo: Research more on how to customize a node settings
-	node, err := libp2p.New()
+
+	//here we will adjust the ping service of the node
+	node, err := libp2p.New(
+		libp2p.Ping(false),
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -27,12 +35,54 @@ func main (){
 		"Host Addr", hostInfo.Addrs,
 	)
 
-	//lets wait for a shutdown signal to close the connection
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	<-ch
+	//configuration of our own ping service
+	pingService := &ping.PingService{Host: node}
+	node.SetStreamHandler(ping.ID,pingService.PingHandler)
 
-	sugarLogger.Infoln("Received signal, shutting down...")
+	//here lets generate the p2p address format for conn
+	peerInfo := peerstore.AddrInfo{
+		ID: node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	sugarLogger.Infoln("node address formated: ", addrs[0])
+
+	
+
+	//here we will try to add a commandline arg to capture the node addr to be connected to
+	if len(os.Args) > 1 {
+		addr, err := multiaddr.NewMultiaddr(os.Args[1])
+		if err != nil {
+			sugarLogger.Panic(err)
+		}
+
+		//create a peer AddrInfo from the addr string captured
+		peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+		if err != nil {
+			sugarLogger.Panic(err)
+		}
+
+		//@todo: Add context timeout to provide timeoutes for conn establishement
+		if err := node.Connect(context.Background(), *peer); err != nil {
+			sugarLogger.Panic(err)
+		}
+		sugarLogger.Infoln("pinging 5 messages to: ", addr)
+		//@todo: context timeout to provide timeouts for conn establishment
+		ch := pingService.Ping(context.Background(), peer.ID)
+		for i := 0; i < 5; i++ {
+			res := <-ch
+			sugarLogger.Infoln("Got response!, round trip: ", res.RTT)
+		}
+
+	} else {
+		//lets wait for a shutdown signal to close the connection
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		<-ch
+		sugarLogger.Infoln("Received signal, shutting down...")
+	}
+
+	
 
 	//shut down the node when an error has occured
 	if err := node.Close(); err != nil {
