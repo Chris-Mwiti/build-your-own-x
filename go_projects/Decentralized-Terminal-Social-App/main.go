81d5fe592b30712be10b7a4b9a13c7536c15a022
Chris-Mwiti <chrismwiti21@gmail.com>
2025-08-14 10:18:52 +0300

    changes: Added cobra cli to manage cli commands adds: Added a node directory to keep track of the p2p node modify: Modification of the ping function to follow separation of concern

--- go_projects/Decentralized-Terminal-Social-App/main.go
+++ go_projects/Decentralized-Terminal-Social-App/main.go
@@ -1,91 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
+import "github.com/Chris-Mwiti/ChatX/cmd"
 
-	"github.com/Chris-Mwiti/ChatX/logger"
-	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/core/host"
-	peerstore "github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
-	"github.com/multiformats/go-multiaddr"
-)
-
-func main (){
-	//logger initialization
-	_, sugarLogger := logger.Init()
-
-	//now lets try to reconfigure the node ip address
-	//@todo: Research more on how to customize a node settings
-
-	//here we will adjust the ping service of the node
-	node, err := libp2p.New(
-		libp2p.Ping(false),
-	)
-	if err != nil {
-		panic(err)
-	}
-	hostInfo := host.InfoFromHost(node)
-
-	sugarLogger.Infow("Host info: ", 
-		"Host Id", hostInfo.ID,
-		"Host Addr", hostInfo.Addrs,
-	)
-
-	//configuration of our own ping service
-	pingService := &ping.PingService{Host: node}
-	node.SetStreamHandler(ping.ID,pingService.PingHandler)
-
-	//here lets generate the p2p address format for conn
-	peerInfo := peerstore.AddrInfo{
-		ID: node.ID(),
-		Addrs: node.Addrs(),
-	}
-	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
-	sugarLogger.Infoln("node address formated: ", addrs[0])
-
-	
-
-	//here we will try to add a commandline arg to capture the node addr to be connected to
-	if len(os.Args) > 1 {
-		addr, err := multiaddr.NewMultiaddr(os.Args[1])
-		if err != nil {
-			sugarLogger.Panic(err)
-		}
-
-		//create a peer AddrInfo from the addr string captured
-		peer, err := peerstore.AddrInfoFromP2pAddr(addr)
-		if err != nil {
-			sugarLogger.Panic(err)
-		}
-
-		//@todo: Add context timeout to provide timeoutes for conn establishement
-		if err := node.Connect(context.Background(), *peer); err != nil {
-			sugarLogger.Panic(err)
-		}
-		sugarLogger.Infoln("pinging 5 messages to: ", addr)
-		//@todo: context timeout to provide timeouts for conn establishment
-		ch := pingService.Ping(context.Background(), peer.ID)
-		for i := 0; i < 5; i++ {
-			res := <-ch
-			sugarLogger.Infoln("Got response!, round trip: ", res.RTT)
-		}
-
-	} else {
-		//lets wait for a shutdown signal to close the connection
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-		<-ch
-		sugarLogger.Infoln("Received signal, shutting down...")
-	}
-
-	
-
-	//shut down the node when an error has occured
-	if err := node.Close(); err != nil {
-		panic(err)
-	}
+func main() {
+	cmd.Execute()
 }
---
+++ go_projects/Decentralized-Terminal-Social-App/nodes/node.go
@@ -0,0 +1,91 @@
+package nodes
+
+import (
+	"context"
+	"time"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+	"github.com/multiformats/go-multiaddr"
+	"go.uber.org/zap"
+	"golang.org/x/tools/go/analysis/passes/defers"
+)
+
+func Start(logger *zap.SugaredLogger)(host.Host){
+	logger.Infoln("Staring node...")
+	node, err := libp2p.New()
+
+	if err != nil {
+		logger.Fatalf("Could not be able to start node: %v\n", err)
+	}
+
+	logger.Infoln("Node created!")
+
+	return node
+}
+
+func Close(logger *zap.SugaredLogger, node host.Host){
+	logger.Infoln("Shutting down node...")
+	err := node.Close()
+
+	if err != nil {
+		logger.Fatalf("Could not be able to shutdown node: %v\n", err)
+	}
+
+	logger.Infoln("Node shutdown!")
+}
+
+
+
+func Ping(logger *zap.SugaredLogger, peerAddr string)(error){
+	node := Start(logger)
+	defer Close(logger, node)
+	//here lets generate the node info
+	hostInfo := peerstore.AddrInfo{
+		ID: node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&hostInfo)
+	if err != nil {
+		logger.Errorf("Error while creating host addr from info: %s", err.Error())
+		return err
+	}
+	logger.Infow("Host Info: ", "Address", addrs, "ID", hostInfo.ID)
+	
+	//configure our own ping protocol
+	pingService := &ping.PingService{Host: node}
+	node.SetStreamHandler(ping.ID, pingService.PingHandler)
+
+
+	//here lets format the input arg to multiaddr 
+	addr, err := multiaddr.NewMultiaddr(peerAddr)
+	if err != nil {
+		logger.Errorf("Error while formating addr format: %s", err.Error())
+		return err
+	}
+	//create a peer info from the addr
+	peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+	if err != nil {
+		logger.Errorf("Error while creating peer info: %s", err.Error())
+		return err
+	}
+
+	//establish a connection from the host to peer
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Minute)
+	defer cancel()
+	if err := node.Connect(ctx, *peer); err != nil {
+		logger.Errorf("Error while pinging peer %s: ", err.Error())
+		cancel()
+	}
+
+	//ping to check if there's a connection
+	ch := pingService.Ping(ctx, peer.ID)
+	for i := 0; i < 5; i++ {
+		res := <-ch
+		logger.Infoln("ping response. RTT: ", res.RTT)
+	}
+
+	return nil
+}
