2fd4638440e1c6475ca242ff06000e7395e4bdb3
Chris-Mwiti <chrismwiti21@gmail.com>
2025-09-06 16:06:21 +0300

    changes-demo: Apply the abstracted functionality to the main entry point of the file

--- go_projects/orchestra/main.go
+++ go_projects/orchestra/main.go
@@ -3,20 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
-	"github.com/Chris-Mwiti/build-your-own-x/go_projects/orchestra/manager"
-	"github.com/Chris-Mwiti/build-your-own-x/go_projects/orchestra/node"
 	"github.com/Chris-Mwiti/build-your-own-x/go_projects/orchestra/task"
-	"github.com/Chris-Mwiti/build-your-own-x/go_projects/orchestra/worker"
 	"github.com/docker/docker/client"
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
 )
 
 func createContainer () (*task.Docker, *task.DockerResult) {
 	c := task.Config{
-		Name: "test-container-w",
+		Name: "postgres-container",
 		Image: "postgres",
 		Env: []string{
 			"POSTGRES_USER=cube",
@@ -58,59 +54,20 @@ func stopContainer(d *task.Docker, containerId string) (*task.DockerResult) {
 func main(){
 	fmt.Println("Upcoming orchestra project")
 
-	taskOne := task.Task{
-		ID: uuid.New(),
-		Name: "Task-one",
-		State: task.Pending,
-		Image: "Image One",
-		Memory: 1024,
-		Disk: 1,
-	}
-
-	taskEvent := task.TaskEvent{
-		ID: uuid.New(),
-		State: task.Pending,
-		Task: taskOne,
-		Timestamp: time.Now(),
-	}
-
-	fmt.Printf("task: %v\n", taskOne)
-	fmt.Printf("task event: %v\n", taskEvent)
-
-	worker := worker.Worker{
-		Name: "worker-1",
-		Queue: *queue.New(),
-		Db: make(map[uuid.UUID]*task.Task),
-	}
-
-	fmt.Printf("struct worker: %v\n", worker)
-	worker.RunTask()
-	worker.StartTask()
-	worker.StopTask()
-	worker.CollectStats()
+	fmt.Println("Demo: Creating a container")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v", createResult.Error)
+		os.Exit(1)
+	}	
 	
-	manager := manager.Manager{
-		Pending: *queue.New(),
-		TasksDb: make(map[string][]*task.Task),
-		TasksEventDb: make(map[string][]*task.TaskEvent),
-		Workers: []string{worker.Name},
-		WorkerTaskMap: make(map[string][]uuid.UUID),
+	//simulate a sleep time
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Demo: Stopping container %s\n", createResult.ContainerId)
+
+	deleteResult := stopContainer(dockerTask, createResult.ContainerId)
+	if deleteResult.Error != nil {
+		log.Fatalf("%v\n", deleteResult.Error)
 	}
-
-	fmt.Printf("struct manager: %v\n", manager)
-	manager.SelectWorker()
-	manager.SendWork()
-	manager.UpdateTask()
-
-
-	node := node.Node{
-		Name: "Node-1",
-		Ip: "192.168.1.0",
-		Cores: 4,
-		Memory: 1024,
-		Disk: 25,
-		Role: "Worker",
-	}
-
-	fmt.Printf("struct node: %v\n", node)
+	log.Printf("%v\n", deleteResult)
 }
