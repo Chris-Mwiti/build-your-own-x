a4008c03047b45810290fddbe1759236acd2c940
Chris-Mwiti <chrismwiti21@gmail.com>
2025-09-09 10:00:07 +0300

    changes: Implemented the run to command for the workers

--- go_projects/orchestra/worker/worker.go
+++ go_projects/orchestra/worker/worker.go
@@ -1,8 +1,10 @@
 package worker
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"slices"
 	"time"
 
 	"github.com/Chris-Mwiti/build-your-own-x/go_projects/orchestra/task"
@@ -11,11 +13,31 @@ import (
 	"github.com/google/uuid"
 )
 
+//state machine for the task state transition
+var stateTransitionMap = map[taskModule.State][]taskModule.State{
+	taskModule.Pending: {taskModule.Scheduled},
+	taskModule.Scheduled: {taskModule.Scheduled,taskModule.Runnig, taskModule.Failed},
+	taskModule.Runnig: {taskModule.Runnig,taskModule.Completed,taskModule.Failed},
+	taskModule.Completed: {},
+	taskModule.Failed: {},
+}
+
+//state machine helper functions (engine)
+func contains(states []taskModule.State, state taskModule.State) bool {
+	if ok := slices.Contains(states, state); ok{
+		return true
+	}	
+	return false
+}
+
+func ValidStateTransition(src taskModule.State, dst taskModule.State) bool {
+	return contains(stateTransitionMap[src], dst)
+}
 //represent the attribute and methods available for the worker
 type Worker struct {
 	Name string
 	Queue queue.Queue //to keep track of pending tasks
-	Db map[uuid.UUID]*task.Task //store the processed tasks and their states
+	Db map[uuid.UUID]*taskModule.Task //store the processed tasks and their states
 	TaskCount int //count of the tasks assigned to the worker
 }
 
@@ -26,12 +48,46 @@ func (worker *Worker) CollectStats(){
 }
 
 //determine the state of a task & actions: start & stop a task based on their state
-func (worker *Worker) RunTask(){
-	fmt.Println("Running task...")
+func (worker *Worker) RunTask() taskModule.DockerResult{
+	//here we deque the first task to be uploaded & process
+	t := worker.Queue.Dequeue()
+	if t == nil {
+		log.Println("No tasks in the queue")
+		return taskModule.DockerResult{
+			Error: nil,
+		}
+	}
+	//type assertion from interface to task
+	taskQueued := t.(taskModule.Task)
+	//fetch the earlier version
+	taskPersisted := worker.Db[taskQueued.ID]
+
+	if taskPersisted == nil {
+		taskPersisted = &taskQueued
+		worker.Db[taskQueued.ID] = &taskQueued
+	}
+	var result taskModule.DockerResult
+
+	if ValidStateTransition(taskPersisted.State, taskQueued.State){
+		switch taskQueued.State {
+		case taskModule.Scheduled:
+			result = worker.StartTask(&taskQueued)
+		case taskModule.Completed:
+			result = worker.StopTask(&taskQueued)
+		default:
+			result.Error = errors.New("Not supported state transition")
+		}
+	} else {
+		err := fmt.Errorf("Invalid state transition from %s, to %s", taskPersisted.State, taskQueued.State)
+		result.Error = err
+	}
+
+	return result
 }
 
+
 //actions: start a task
-func (worker *Worker) StartTask(task *taskModule.Task)(task.DockerResult){
+func (worker *Worker) StartTask(task *taskModule.Task)(taskModule.DockerResult){
 	log.Printf("starting task %s", task.ID)
 
 	//get the config of the task
@@ -54,14 +110,15 @@ func (worker *Worker) StartTask(task *taskModule.Task)(task.DockerResult){
 
 	log.Println("Running: Completed running task")
 	task.State = taskModule.Runnig
-	worker.Db[task.ID] = task
 	//setting the taskt runtime Id
 	task.ContainerId = result.ContainerId
+
+	worker.Db[task.ID] = task
 	return result
 }
 
 //actions: stop a task
-func (worker *Worker) StopTask(task *taskModule.Task)(task.DockerResult){
+func (worker *Worker) StopTask(task *taskModule.Task)(taskModule.DockerResult){
 	log.Printf("Stoping task... %s\n", task.ID)
 	taskCfg := taskModule.NewConfig(task) 
 	dockerClient, err:= taskModule.NewDocker(*taskCfg)
@@ -98,6 +155,16 @@ func (worker *Worker) StopTask(task *taskModule.Task)(task.DockerResult){
 		Action: "stop_task",
 		Result: "success",
 		ContainerId: result.ContainerId,
+		Error: nil,
+	}
+}
+
+//add the task to the queue for execution
+func (w *Worker) AddTask(task taskModule.Task) taskModule.DockerResult{
+	w.Queue.Enqueue(task)
+	return taskModule.DockerResult{
+		Action: "TaskAdd",
+		Result: "success",
 		Error: nil,
 	}
 }
