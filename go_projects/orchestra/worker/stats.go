02be2be823353e25fbe9717ae17527af501d639b
Chris-Mwiti <chrismwiti21@gmail.com>
2025-09-17 15:09:27 +0300

    changes: Implemented a cpu stat perc calc wrapper

---
+++ go_projects/orchestra/worker/stats.go
@@ -0,0 +1,66 @@
+package worker
+
+import (
+	"github.com/c9s/goprocinfo/linux"
+)
+
+type CPUStat struct {
+	Id string `json:"id"`
+	User uint64 `json:"user"`
+	Nice uint64 `json:"nice"`
+	System uint64 `json:"system"`
+	Idle uint64 `json:"idle"`
+	IOWait uint64 `json:"iowait"`
+	IRQ uint64 `json:"irq"`
+	SoftIRQ uint64 `json:"softirq"`
+	Steal uint64 `json:"steal"`
+	Guest uint64 `json:"guest"`
+	GuestNice uint64 `json:"guest_nice"`
+}
+
+
+
+//this struct keeps of the imp stats of the node the worker is running on
+type Stats struct {
+	MemStats *linux.MemInfo
+	DiskStats *linux.Disk
+	CpuStats *CPUStat
+	LoadStats *linux.LoadAvg
+}
+
+//get the total memory of the worker machine
+//@todo: remember to add the kb at the end off the num
+func (s *Stats) MemTotalKb() uint64 {
+	return s.MemStats.MemTotal
+}
+
+func (s *Stats) MemAvailbeKb() uint64 {
+	return s.MemStats.MemAvailable
+}
+
+func (s *Stats) MemUsedKb() uint64 {
+	return s.MemStats.MemTotal - s.MemStats.MemAvailable
+}
+
+func (s *Stats) MemUsedPercent() uint64 {
+	return s.MemStats.MemAvailable / s.MemStats.MemTotal
+}
+
+
+//steps to change cpu usage
+//1. Sum values of idle states
+//2. sum values of the non-idle states
+//3. sum of total idle and non-idle states
+//4. sub the idle from total and divide the result by the total
+func (s *Stats) CpuUsage() float64 {
+
+	idle := s.CpuStats.Idle + s.CpuStats.IOWait
+	nonIdle := s.CpuStats.User + s.CpuStats.Nice + s.CpuStats.System + s.CpuStats.IRQ + s.CpuStats.SoftIRQ + s.CpuStats.Steal
+	total := idle + nonIdle
+
+	if total == 0 {
+		return 0.00
+	}
+
+	return (float64(total) - float64(idle)) / float64(total)
+}
